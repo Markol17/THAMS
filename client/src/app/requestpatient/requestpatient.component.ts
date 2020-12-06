1a8477367e73d7a56ba43349c51cc13bf6c38f41
Alex Degrace <adegr091@uottawa.ca>
2020-12-06 17:17:09 -0500

    Started requestPatient

--- client/src/app/requestpatient/requestpatient.component.ts
+++ client/src/app/requestpatient/requestpatient.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestpatientComponent implements OnInit {
 
+  patientId:number;
+  divisionId:number;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addToRequestList(){
+
   }
 
 }
