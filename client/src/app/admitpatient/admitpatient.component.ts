856392a2ea8ff00f832226c31683e175e1582529
Alex Degrace <adegr091@uottawa.ca>
2020-12-08 22:00:44 -0500

    fix admit

--- client/src/app/admitpatient/admitpatient.component.ts
+++ client/src/app/admitpatient/admitpatient.component.ts
@@ -27,13 +27,8 @@ export class AdmitpatientComponent implements OnInit {
     this.admission.requestList(this.divisionId).valueChanges.subscribe({
       next: (data) => {
         console.log(data);
-<<<<<<< HEAD
         var temp= data.data['requestList'];
         var temp2 = temp['patients'];
-=======
-        var temp = data.data["requestList"];
-        var temp2 = temp["patient"];
->>>>>>> 3a39c430079ae3b994dbbfcbe00bd01a7bcf0dec
         console.log(temp2);
         this.patientList.splice(0, this.patientList.length);
         temp2.forEach((element) => {
@@ -57,7 +52,6 @@ export class AdmitpatientComponent implements OnInit {
     });
   }
 
-<<<<<<< HEAD
   admit(id :number){
 
     this.admission.divisionInfo(this.divisionId).valueChanges.subscribe({ next: data => {
@@ -81,29 +75,5 @@ export class AdmitpatientComponent implements OnInit {
       this.customMessageService.setError(err);
     } } );
     
-=======
-  admit(id: number) {
-    this.admission.divisionInfo(this.divisionId).valueChanges.subscribe({
-      next: (data) => {
-        this.division = null;
-        console.log(data);
-        const x = data.data["divisionInfo"];
-        console.log(x);
-        let jsonObj: any = JSON.parse(JSON.stringify(x));
-        this.division = <Division>jsonObj;
-        console.log(this.division);
-        if (!this.division.isComplete) {
-          this.admission.admitPatient(id, this.divisionId, this.division);
-        } else if (this.division.isComplete) {
-          console.log("No more space in the division");
-          this.customMessageService.setError("No more space in the division");
-        }
-      },
-      error: (err) => {
-        console.error("Error admiting patient: " + err);
-        this.customMessageService.setError(err);
-      },
-    });
->>>>>>> 3a39c430079ae3b994dbbfcbe00bd01a7bcf0dec
   }
 }
