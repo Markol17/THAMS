432723621b11f382ec0f318612da994fa9b40234
Markol17 <markolivierpoulin@hotmail.com>
2020-12-09 00:03:25 -0500

    fixed date validation

--- client/src/app/services/prescription.service.ts
+++ client/src/app/services/prescription.service.ts
@@ -1,39 +1,35 @@
-import { Injectable } from '@angular/core';
-import { Apollo } from 'apollo-angular';
+import { Injectable } from "@angular/core";
+import { Apollo } from "apollo-angular";
 import { addPrescription } from "../gql/mutation";
 import { patientPrescriptions } from "../gql/query";
-import { AddPrescriptionInput } from '../objects/add-prescription-input.model';
-import { PatientIdInput } from '../objects/patient-id-input.model';
+import { AddPrescriptionInput } from "../objects/add-prescription-input.model";
+import { PatientIdInput } from "../objects/patient-id-input.model";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PrescriptionService {
-
-  constructor(private apollo: Apollo) { }
+  constructor(private apollo: Apollo) {}
   pId: PatientIdInput;
-
 
   addPrescription(prescription: AddPrescriptionInput): any {
     return this.apollo.mutate({
       mutation: addPrescription,
       variables: {
-        prescription: prescription
-      }
-    })
+        prescription: prescription,
+      },
+    });
   }
 
-  
   getPrescriptions(id: number): any {
-    this.pId = new PatientIdInput;
+    this.pId = new PatientIdInput();
     this.pId.patientId = id;
-    
+
     return this.apollo.watchQuery<any>({
-      fetchPolicy: 'no-cache',
+      fetchPolicy: "no-cache",
       query: patientPrescriptions,
       variables: {
-        id:this.pId
-      }
-    })
+        id: this.pId,
+      },
+    });
   }
-
 }
