eea2ab21f962bc98a2a80392065cabf6aa81bc8e
Markol17 <markolivierpoulin@hotmail.com>
2020-12-08 20:45:49 -0500

    formatted components

--- client/src/app/admitpatient/admitpatient.component.ts
+++ client/src/app/admitpatient/admitpatient.component.ts
@@ -1,76 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { MessageService } from 'primeng/api';
-import { Division } from '../objects/division.model';
-import { Patient } from '../objects/patient.model';
-import { AdmissionserviceService } from '../services/admissionservice.service';
-import { CustomMessageService } from '../services/message.service';
+import { Component, OnInit } from "@angular/core";
+import { Division } from "../objects/division.model";
+import { Patient } from "../objects/patient.model";
+import { AdmissionserviceService } from "../services/admissionservice.service";
+import { CustomMessageService } from "../services/message.service";
 
 @Component({
-  selector: 'app-admitpatient',
-  templateUrl: './admitpatient.component.html',
-  styleUrls: ['./admitpatient.component.css']
+  selector: "app-admitpatient",
+  templateUrl: "./admitpatient.component.html",
+  styleUrls: ["./admitpatient.component.css"],
 })
 export class AdmitpatientComponent implements OnInit {
-
   divisionId: number;
   patientList: Patient[];
   division: Division;
 
-  constructor(private admission : AdmissionserviceService, private customMessageService: CustomMessageService) { }
+  constructor(
+    private admission: AdmissionserviceService,
+    private customMessageService: CustomMessageService
+  ) {}
 
   ngOnInit() {
     this.patientList = [];
   }
 
-  getRequestList(){
+  getRequestList() {
     this.admission.requestList(this.divisionId).valueChanges.subscribe({
-      next: data => {
+      next: (data) => {
         console.log(data);
-        var temp= data.data['requestList'];
-        var temp2 = temp['patient'];
+        var temp = data.data["requestList"];
+        var temp2 = temp["patient"];
         console.log(temp2);
-        this.patientList.splice(0,this.patientList.length);
-        temp2.forEach(element => {
+        this.patientList.splice(0, this.patientList.length);
+        temp2.forEach((element) => {
           let jsonObj: any = JSON.parse(JSON.stringify(element));
           let currentPatient = <Patient>jsonObj;
           this.patientList.push(currentPatient);
         });
-        if(this.patientList && this.patientList.length == 0){
-          this.customMessageService.setError("No patient in the division admit list");
+        if (this.patientList && this.patientList.length == 0) {
+          this.customMessageService.setError(
+            "No patient in the division admit list"
+          );
         }
-        console.log("patientList: "+this.patientList);
+        console.log("patientList: " + this.patientList);
       },
-      error: err => {
-        console.error('Error getting request list: ' + err);
-        this.customMessageService.setError("Could not get the request list beacause of " + err);
+      error: (err) => {
+        console.error("Error getting request list: " + err);
+        this.customMessageService.setError(
+          "Could not get the request list beacause of " + err
+        );
       },
-
-    });;
-    
+    });
   }
 
-  admit(id :number){
-
-    this.admission.divisionInfo(this.divisionId).valueChanges.subscribe({ next: data => {
-      this.division = null;
-      console.log(data);
-      const x = data.data['divisionInfo'];
-      console.log(x);
-      let jsonObj: any = JSON.parse(JSON.stringify(x));
-      this.division = <Division>jsonObj;
-      console.log(this.division);
-      if(!this.division.isComplete){
-        this.admission.admitPatient(id,this.divisionId,this.division);
-      }else if(this.division.isComplete){
-        console.log("No more space in the division")
-        this.customMessageService.setError("No more space in the division");
-      }
-    },
-    error: err => {
-      console.error('Error admiting patient: ' + err);
-      this.customMessageService.setError(err);
-    } } );
-    
+  admit(id: number) {
+    this.admission.divisionInfo(this.divisionId).valueChanges.subscribe({
+      next: (data) => {
+        this.division = null;
+        console.log(data);
+        const x = data.data["divisionInfo"];
+        console.log(x);
+        let jsonObj: any = JSON.parse(JSON.stringify(x));
+        this.division = <Division>jsonObj;
+        console.log(this.division);
+        if (!this.division.isComplete) {
+          this.admission.admitPatient(id, this.divisionId, this.division);
+        } else if (this.division.isComplete) {
+          console.log("No more space in the division");
+          this.customMessageService.setError("No more space in the division");
+        }
+      },
+      error: (err) => {
+        console.error("Error admiting patient: " + err);
+        this.customMessageService.setError(err);
+      },
+    });
   }
-
 }
--- client/src/app/consultpatient/consultpatient.component.ts
+++ client/src/app/consultpatient/consultpatient.component.ts
@@ -1,124 +1,113 @@
-import { Component, OnInit } from '@angular/core';
-import { PatientService } from '../services/patient.service';
-import {Patient} from '../objects/patient.model'
-import { CustomMessageService } from '../services/message.service';
-import { timeStamp } from 'console';
-import { patients } from '../gql/query';
-import { AuthenticationService } from '../services/authentication.service';
-import { SelectItem } from 'primeng/api';
-import { UpdatePatient } from '../objects/update-patient.model';
-
+import { Component, OnInit } from "@angular/core";
+import { PatientService } from "../services/patient.service";
+import { Patient } from "../objects/patient.model";
+import { CustomMessageService } from "../services/message.service";
+import { AuthenticationService } from "../services/authentication.service";
+import { SelectItem } from "primeng/api";
+import { UpdatePatient } from "../objects/update-patient.model";
 
 @Component({
-  selector: 'app-consultpatient',
-  templateUrl: './consultpatient.component.html',
-  styleUrls: ['./consultpatient.component.css']
+  selector: "app-consultpatient",
+  templateUrl: "./consultpatient.component.html",
+  styleUrls: ["./consultpatient.component.css"],
 })
-
 export class ConsultpatientComponent implements OnInit {
-  patient: Patient ;
+  patient: Patient;
   updatePatient: UpdatePatient;
-  id:number;
-  submitted:boolean;
-  edit:boolean=false;
+  id: number;
+  submitted: boolean;
+  edit: boolean = false;
   role: string;
   maritals: SelectItem[];
   genders: SelectItem[];
-  
-  constructor(private patientserice: PatientService,private customMessageService: CustomMessageService, private authservice: AuthenticationService ) {
-   this.updatePatient = new UpdatePatient;
+
+  constructor(
+    private patientserice: PatientService,
+    private customMessageService: CustomMessageService,
+    private authservice: AuthenticationService
+  ) {
+    this.updatePatient = new UpdatePatient();
     this.genders = [
       { label: "male", value: "Male" },
       { label: "female", value: "Female" },
-      { label: "I would rather not say", value: "I would rather not say" }
+      { label: "I would rather not say", value: "I would rather not say" },
     ];
 
     this.maritals = [
       { label: "single", value: "single" },
       { label: "married", value: "married" },
       { label: "widowed", value: "widowed" },
-      { label: "divorced", value: "divorced"},
-      { label: "separated", value: "separated"},
-      { label: "registered partnership", value: "registered partnership"}
-
-    ]
+      { label: "divorced", value: "divorced" },
+      { label: "separated", value: "separated" },
+      { label: "registered partnership", value: "registered partnership" },
+    ];
   }
   ngOnInit() {
     this.role = this.authservice.getRole();
   }
 
-  isNurseAndAdmitted():boolean{
-    if(this.role == "ChargeNurse" && this.patient.isAdmitted){
+  isNurseAndAdmitted(): boolean {
+    if (this.role == "ChargeNurse" && this.patient.isAdmitted) {
       return true;
     }
     return false;
   }
 
-  isDoctor():boolean{
-    return this.role=="Doctor";
+  isDoctor(): boolean {
+    return this.role == "Doctor";
   }
 
-  findPatient(): void{
-   this.patientserice.patientInfo(this.id).valueChanges.subscribe({
-      next: data => {
+  findPatient(): void {
+    this.patientserice.patientInfo(this.id).valueChanges.subscribe({
+      next: (data) => {
         console.log(data);
-        const x = data.data['patientInfo'];
-        let jsonObj: any = JSON.parse(JSON.stringify(x['patient']));
+        const x = data.data["patientInfo"];
+        let jsonObj: any = JSON.parse(JSON.stringify(x["patient"]));
         this.patient = <Patient>jsonObj;
-        if (this.patient!=null) {
-          this.customMessageService.setSuccess("Here is the file of  "+this.patient.firstName);
-          this.updatePatient.patientId = this.patient.id; 
+        if (this.patient != null) {
+          this.customMessageService.setSuccess(
+            "Here is the file of  " + this.patient.firstName
+          );
+          this.updatePatient.patientId = this.patient.id;
           this.updatePatient.address = this.patient.address;
           this.updatePatient.phoneNumber = this.patient.phoneNumber;
           this.updatePatient.maritalStatus = this.patient.maritalStatus;
           this.updatePatient.gender = this.patient.gender;
           this.updatePatient.nextOfKin = this.patient.nextOfKin;
           this.updatePatient.privateInsuranceNumber = this.patient.privateInsuranceNumber;
-          this.submitted=true; 
-          
-        }
-        else {
+          this.submitted = true;
+        } else {
           this.customMessageService.setError("Coulnd't find this patient");
-          this.submitted=false;
-         
+          this.submitted = false;
         }
       },
-      error: err => { console.error('Patient search error:' + err);
-     this.customMessageService.setError("Coulnd't find this patient");
-     this.submitted=false;
-   
+      error: (err) => {
+        console.error("Patient search error:" + err);
+        this.customMessageService.setError("Coulnd't find this patient");
+        this.submitted = false;
+      },
+    });
+  }
 
+  updatePatientInfo(): void {
+    if (Object.values(this.updatePatient).every((o) => o != "")) {
+      this.patientserice.updatePatient(this.updatePatient).subscribe({
+        next: (data) => {},
+        error: (err) => {
+          console.error("Error updating Patient: " + err);
+          this.customMessageService.setError(
+            "There was an error updating this patient"
+          );
+        },
+        complete: () => {
+          this.customMessageService.setSuccess("Patient file updated");
+          this.edit = false;
+        },
+      });
+    } else {
+      this.customMessageService.setError(
+        "One of the field you editted is empty"
+      );
     }
-    });;
   }
-
-  updatePatientInfo(): void{
-
-    if(Object.values(this.updatePatient).every(o => o != "")){
-
-    this.patientserice.updatePatient(this.updatePatient).subscribe({
-      next: data => {
-        
-
-      },
-      error: err => {
-        console.error('Error updating Patient: ' + err);
-        this.customMessageService.setError("There was an error updating this patient");
-      },
-      complete: () => { 
-        this.customMessageService.setSuccess("Patient file updated");
-        this.edit=false;
-      },
-
-
-    });;
-  }
-  else{
-    this.customMessageService.setError("One of the field you editted is empty");
-
-  }
-    
-  }
-
-
 }
